a9f2bafbd52eff2ccc603db0363642f27dd81551
PrimRGB <primrgb@gmail.com>
2020-03-26 15:51:01 +0200

    ExplorationStart

--- src/exploration_utils/plot_utils.py
+++ src/exploration_utils/plot_utils.py
@@ -5,7 +5,19 @@ import pandas as pd
 def category_count_per_label_subplots(df: pd.DataFrame, category_feature_name: str, label_feature_name: str,
                                       nrows: int, ncols: int, figsize: Tuple, wspace: float,
                                       ticks: List[int], ticklabels: List):
-
+    '''
+    Plots multiple subplots - each subplot represents a different value of the label_feature.
+    :param df: Pandas DataFrame containing both category_feature_name and label_feature_name.
+    :param category_feature_name: The feature to plot for each slice
+    :param label_feature_name: The feature whose values slice our dataframe, creating different plots for each value.
+    :param nrows: Number of subplots rows.
+    :param ncols: Number of subplots columns.
+    :param figsize: Size of the complete figure.
+    :param wspace: Spacing parameter between each pair of subplots.
+    :param ticks: Ticks for the bar plot.
+    :param ticklabels: Ticks labels for the bar plot.
+    :return:
+    '''
     labels = df[label_feature_name].unique()
     fig, axes = plt.subplots(nrows, ncols, figsize=figsize)
     fig.subplots_adjust(wspace=wspace)
@@ -16,6 +28,7 @@ def category_count_per_label_subplots(df: pd.DataFrame, category_feature_name: s
                  width=counts)
         ax.set_xticks(ticks=ticks)
         ax.set_xticklabels(labels=ticklabels)
+        ax.set_title('{0} = {1}'.format(label_feature_name,str(labels[i])))
         ax.grid(which='major', axis='x')
 
 def category_count_per_label_plot(df: pd.DataFrame, category_feature_name: str, label_feature_name: str):
