57fef2bbb81c3c9594f3279aaf426b28fddf4536
PrimRGB <primrgb@gmail.com>
2020-03-31 12:44:18 +0300

    TransformerFix

--- src/transformation_utils/transformers.py
+++ src/transformation_utils/transformers.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from typing import List
 
-class versionTransformer(BaseEstimator, TransformerMixin):
+class VersionToNum(BaseEstimator, TransformerMixin):
 
     def __init__(self, version_column: str):
         self.version_column = version_column
@@ -13,16 +13,15 @@ class versionTransformer(BaseEstimator, TransformerMixin):
                                      for i in range(self.subversions_count)])
         return self
 
-    def add_digits_from_list(self, verlist: List[str], add_digits: int):
+    def add_digits_from_list(self, verlist: List[str]):
         new_vers = []
         for ver in verlist:
-            extra_digits_count = add_digits - len(ver)
+            extra_digits_count = self.max_digits_count - len(ver)
             new_vers.append('0'*extra_digits_count + ver)
         return ''.join(new_vers)
 
     def transform(self, X, y=None):
-        return X[self.version_column].apply(lambda x: int(self.add_digits_from_list(x.split('.'),
-                                                          add_digits = self.max_digits_count)))
+        return X[self.version_column].apply(lambda x: int(self.add_digits_from_list(x.split('.'))))
 
 
 
